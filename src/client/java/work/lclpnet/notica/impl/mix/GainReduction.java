30d0958bb2aeb529e2b0321a98d7ffbb5ee1d2f2
LCLP <lukas@lclpnet.work>
2025-05-08 16:08:53 +0200

    fix logarithmic conversion

--- src/client/java/work/lclpnet/notica/impl/mix/GainReduction.java
+++ src/client/java/work/lclpnet/notica/impl/mix/GainReduction.java
@@ -1,6 +1,17 @@
 package work.lclpnet.notica.impl.mix;
 
 import org.jetbrains.annotations.Nullable;
+import work.lclpnet.notica.util.ByteBufferInputStream;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static java.lang.Math.*;
 import static net.minecraft.util.math.MathHelper.lerp;
@@ -8,10 +19,10 @@ import static net.minecraft.util.math.MathHelper.lerp;
 /// Heavily inspired by the [openal-soft compressor](https://github.com/kcat/openal-soft/blob/master/core/mastering.cpp).
 public class GainReduction {
 
-    private final float threshold;
+    private final float thresholdLn;
     private final float slope;
     private final float crestCoeff;
-    private final float gainEstimate;
+    private final float gainEstimateLn;
     private final float adaptCoeff;
     private final float attackSamples;
     private final float releaseSamples;
@@ -27,7 +38,7 @@ public class GainReduction {
     public GainReduction(int sampleRate, float lookaheadSec, float thresholdDb, float attackSec, float releaseSec,
                          float holdSec, float ratio, float crestReleaseSec, float adaptationSec) {
 
-        this.threshold = (float) log(10f) * 0.05f * thresholdDb;
+        this.thresholdLn = (float) log(10f) * 0.05f * thresholdDb;
         this.attackSamples = max(1.f, attackSec * sampleRate);
         this.releaseSamples = max(1.f, releaseSec * sampleRate);
         this.lookaheadSamples = max(0, round(lookaheadSec * sampleRate));
@@ -35,7 +46,7 @@ public class GainReduction {
 
         this.crestCoeff = (float) exp(-1.f / (crestReleaseSec * (float) sampleRate));
         this.adaptCoeff = (float) exp(-1.f / (adaptationSec * (float) sampleRate));
-        this.gainEstimate = thresholdDb * -0.5f * slope;
+        this.gainEstimateLn = thresholdLn * -0.5f * slope;
 
         int holdSamples = max(0, round(holdSec * sampleRate));
 
@@ -47,86 +58,85 @@ public class GainReduction {
     }
 
     public void lookAheadGainReduction(float[] interleavedSamples, float[] sideChain) {
-        if (condensator != null) {
-            peakLogCondensateSampleAlongChannels(interleavedSamples, sideChain, condensator);
-        } else {
-            peakLogSampleAlongChannels(interleavedSamples, sideChain);
-        }
+        // sideChain will be in linear domain
+        linkChannels(interleavedSamples, sideChain);
+
+        debugExport(sideChain);
 
         float[] crest = new float[sideChain.length];
 
         calcCrestFactor(sideChain, crest);
 
+        // side chain will be in log domain
+        if (condensator != null) {
+            toLogCondensate(sideChain, condensator);
+        } else {
+            toLog(sideChain);
+        }
+
         compGainReduction(sideChain, crest);
     }
 
-    private void compGainReduction(float[] sideChain, float[] crest) {
-        float releaseEnvelope = lastRelease;
-        float attackEnvelope = lastAttack;
-        float smoothedGainDeviation = lastGainDev;
+    private void debugExportExp(float[] sideChain) {
+        float[] samples = new float[sideChain.length];
 
         for (int i = 0; i < sideChain.length; i++) {
-            // adjust knee width
-            float knee = max(0f, 2.5f * (smoothedGainDeviation * gainEstimate));
-            float kneeHalf = 0.5f * knee;
-
-            // calculate how much the gain should be reduced
-            int lookAheadIndex = min(i + lookaheadSamples, sideChain.length - 1);
-            float overShoot = sideChain[lookAheadIndex] - threshold;  // TODO optimize lookup
-            float gainReduction = calcGainReduction(overShoot, kneeHalf);  // y_G = gain reduction
-
-            // adjust attack and release
-            float crestVal = crest[i];
-            float attackSamples = 2.f * this.attackSamples / crestVal;
-            float attackCoeff = (float) exp(-1.f / attackSamples);
-            float releaseSamples = 2.f * this.releaseSamples / crestVal - attackSamples;
-            float releaseCoeff = (float) exp(-1.f / releaseSamples);
-
-            // adjust reduction depending on current attack/hold/release phase
-            final float attenuation = -slope * gainReduction;
-            releaseEnvelope = max(attenuation, lerp(releaseCoeff, attenuation, releaseEnvelope));
-            attackEnvelope = lerp(attackCoeff, releaseEnvelope, attackEnvelope);
-
-            smoothedGainDeviation = lerp(adaptCoeff, -1f * (attackEnvelope + gainEstimate), smoothedGainDeviation);
-
-            // de-clip
-            smoothedGainDeviation = max(smoothedGainDeviation, sideChain[i] - attackEnvelope - threshold - gainEstimate);
-
-            // post gain
-            float postGain = -(smoothedGainDeviation + gainEstimate);
-
-            sideChain[i] = (float) exp(postGain - attackEnvelope);
+            samples[i] = (float) exp(sideChain[i]);
         }
 
-        // preserve state for next sample
-        lastRelease = releaseEnvelope;
-        lastAttack = attackEnvelope;
-        lastGainDev = smoothedGainDeviation;
+        debugExport(samples);
     }
 
-    private void peakLogCondensateSampleAlongChannels(float[] interleavedSamples, float[] sideChain, Condensator condensator) {
-        for (int i = 0; i < sideChain.length; i++) {
-            // find max abs gain at future sample across all channels
-            float sample = max(abs(interleavedSamples[2 * i]), abs(interleavedSamples[2 * i + 1]));
-            float logSample = (float) log10(max(1e-6f, sample));
+    private void debugExport(float[] samples) {
+        float min = Float.MAX_VALUE, max = Float.MIN_VALUE;
 
-            sideChain[i] = condensator.feed(logSample);
+        for (float sample : samples) {
+            if (sample < min) {
+                min = sample;
+            }
+
+            if (sample > max) {
+                max = sample;
+            }
+        }
+
+        ByteBuffer buf = ByteBuffer.allocate(samples.length * 2).order(ByteOrder.LITTLE_ENDIAN);
+        float range = max - min;
+        float norm = 1f / range;
+
+        for (float sample : samples) {
+            float normedSample = sample * norm;
+            short quantized = (short) clamp(normedSample * Short.MAX_VALUE, Short.MIN_VALUE, Short.MAX_VALUE);
+
+            buf.putShort(quantized);
+        }
+
+        buf.flip();
+
+        var format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
+                48_000, 16, 1, 2, 48_000, false);
+
+        var in = new AudioInputStream(new ByteBufferInputStream(buf), format, buf.limit());
+
+        try (var out = Files.newOutputStream(Path.of("debug-samples.wav"))) {
+            AudioSystem.write(in, AudioFileFormat.Type.WAVE, out);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    private void peakLogSampleAlongChannels(float[] interleavedSamples, float[] sideChain) {
+    private void linkChannels(float[] interleavedSamples, float[] sideChain) {
         for (int i = 0; i < sideChain.length; i++) {
             // find max abs gain at future sample across all channels
-            float sample = max(abs(interleavedSamples[2 * i]), abs(interleavedSamples[2 * i + 1]));
+            float left = abs(interleavedSamples[2 * i]);
+            float right = abs(interleavedSamples[2 * i + 1]);
 
-            // convert to logarithmic
-            sideChain[i] = (float) log10(max(1e-6f, sample));
+            sideChain[i] = max(left, right);
         }
     }
 
     private void calcCrestFactor(float[] sideChain, float[] crest) {
-        // calculate crest factor for each side-chain sample
-
+        // calculate crest factor for each side-chain sample in linear domain
         float peak = this.lastPeak;
         float rms = this.lastRms;
 
@@ -143,13 +153,74 @@ public class GainReduction {
         this.lastRms = rms;
     }
 
+    private void toLog(float[] sideChain) {
+        for (int i = 0; i < sideChain.length; i++) {
+            float sample = sideChain[i];
+
+            sideChain[i] = (float) log(max(1e-6f, sample));
+        }
+    }
+
+    private void toLogCondensate(float[] sideChain, Condensator condensator) {
+        for (int i = 0; i < sideChain.length; i++) {
+            float sample = sideChain[i];
+            float logSample = (float) log(max(1e-6f, sample));
+
+            sideChain[i] = condensator.feed(logSample);
+        }
+    }
+
+    private void compGainReduction(float[] sideChain, float[] crest) {
+        float releaseEnvelope = lastRelease;
+        float attackEnvelope = lastAttack;
+        float smoothedGainDeviation = lastGainDev;
+
+        for (int i = 0; i < sideChain.length; i++) {
+            // adjust knee width
+            float knee = max(0f, 2.5f * (smoothedGainDeviation * gainEstimateLn));
+            float kneeHalf = 0.5f * knee;
+
+            // calculate how much the gain should be reduced
+            int lookAheadIndex = min(i + lookaheadSamples, sideChain.length - 1); // TODO smooth with next sample
+            float overShoot = sideChain[lookAheadIndex] - thresholdLn;  // TODO optimize lookup
+            float gainReduction = calcGainReduction(overShoot, kneeHalf);  // y_G = gain reduction
+
+            // adjust attack and release
+            float crestVal = crest[i];
+            float attackSamples = 2.f * this.attackSamples / crestVal;
+            float attackCoeff = (float) exp(-1.f / attackSamples);
+            float releaseSamples = 2.f * this.releaseSamples / crestVal - attackSamples;
+            float releaseCoeff = (float) exp(-1.f / releaseSamples);
+
+            // adjust reduction depending on current attack/hold/release phase
+            final float attenuation = -slope * gainReduction;
+            releaseEnvelope = max(attenuation, lerp(releaseCoeff, attenuation, releaseEnvelope));
+            attackEnvelope = lerp(attackCoeff, releaseEnvelope, attackEnvelope);
+
+            smoothedGainDeviation = lerp(adaptCoeff, -1f * (attackEnvelope + gainEstimateLn), smoothedGainDeviation);
+
+            // de-clip
+            smoothedGainDeviation = max(smoothedGainDeviation, sideChain[i] - attackEnvelope - thresholdLn - gainEstimateLn);
+
+            // post gain
+            float postGain = -(smoothedGainDeviation + gainEstimateLn);
+
+            sideChain[i] = (float) exp(postGain - attackEnvelope);
+        }
+
+        // preserve state for next sample
+        lastRelease = releaseEnvelope;
+        lastAttack = attackEnvelope;
+        lastGainDev = smoothedGainDeviation;
+    }
+
     private float calcGainReduction(float overShoot, float kneeHalf) {
         if (overShoot <= -kneeHalf) {
             return 0f;
         }
 
         if (overShoot > -kneeHalf && overShoot < kneeHalf) {
-            return 0.5f * (overShoot + kneeHalf) * (overShoot + kneeHalf) / (kneeHalf * 2f);
+            return (overShoot + kneeHalf) * (overShoot + kneeHalf) / (kneeHalf * 4f);
         }
 
         return overShoot;

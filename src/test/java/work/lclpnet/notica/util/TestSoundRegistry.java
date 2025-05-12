3d48518aaa0bf923e864f8870e8a33a13e18737d
LCLP <lukas@lclpnet.work>
2025-05-12 19:16:08 +0200

    make sound mixing supported in headless mode

---
+++ src/test/java/work/lclpnet/notica/util/TestSoundRegistry.java
@@ -0,0 +1,166 @@
+package work.lclpnet.notica.util;
+
+import net.minecraft.client.sound.OggAudioStream;
+import org.jetbrains.annotations.Nullable;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import work.lclpnet.kibu.assets.AssetManager;
+import work.lclpnet.notica.impl.SoundRef;
+import work.lclpnet.notica.impl.SoundSample;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class TestSoundRegistry {
+
+    private final AssetManager assetManager;
+    private JSONObject soundRegistry = new JSONObject();
+
+    public TestSoundRegistry(AssetManager assetManager) {
+        this.assetManager = assetManager;
+    }
+
+    public void init() throws IOException {
+        Path path = assetManager.getAsset("minecraft/sounds.json");
+        Objects.requireNonNull(path, "Asset sounds.json not found");
+
+        String json = Files.readString(path, UTF_8);
+        soundRegistry = new JSONObject(json);
+    }
+
+    public Optional<SoundRef> loadBySoundId(@Nullable String id) {
+        if (id == null) {
+            return Optional.empty();
+        }
+
+        JSONObject cfg = soundRegistry.optJSONObject(id);
+
+        if (cfg == null) {
+            return Optional.empty();
+        }
+
+        JSONArray sounds = cfg.optJSONArray("sounds");
+
+        if (sounds == null || sounds.isEmpty()) {
+            return Optional.empty();
+        }
+
+        Object item = sounds.get(0);
+
+        if (item instanceof String s) {
+            String assetPath = getSoundAssetPath(s);
+            return Optional.of(createRef(assetPath, 1f, 1f));
+        }
+
+        if (!(item instanceof JSONObject sound)) {
+            return Optional.empty();
+        }
+
+        String name = sound.optString("name");
+
+        if (name == null) {
+            return Optional.empty();
+        }
+
+        String assetPath = getSoundAssetPath(name);
+
+        float volume = sound.optFloat("volume", 1f);
+        float pitch = sound.optFloat("pitch", 1f);
+
+        return Optional.of(createRef(assetPath, volume, pitch));
+    }
+
+    private static String getSoundAssetPath(String name) {
+        int sep = name.indexOf(":");
+        String namespace;
+
+        if (sep == -1) {
+            namespace = "minecraft";
+        } else {
+            namespace = name.substring(0, sep);
+            name = name.substring(sep + 1);
+        }
+
+        return namespace + "/sounds/" + name + ".ogg";
+    }
+
+    public boolean has(String name) {
+        return soundRegistry.has(name);
+    }
+
+    public SoundRef createRef(String assetPath, float volume, float pitch) {
+        return new TestRef(assetPath, volume, pitch);
+    }
+
+    private class TestRef implements SoundRef {
+
+        private final String assetPath;
+        private final float volume;
+        private final float pitch;
+
+        private TestRef(String assetPath, float volume, float pitch) {
+            this.assetPath = assetPath;
+            this.volume = volume;
+            this.pitch = pitch;
+        }
+
+        @Override
+        public CompletableFuture<SoundSample> load() {
+            return CompletableFuture.supplyAsync(() -> {
+                try {
+                    return loadSync();
+                } catch (IOException e) {
+                    throw new RuntimeException("Failed to load sound sample", e);
+                }
+            });
+        }
+
+        private SoundSample loadSync() throws IOException {
+            Path path = assetManager.getAsset(assetPath);
+
+            if (path == null) {
+                throw new IllegalStateException("Unknown asset: " + assetPath);
+            }
+
+            try (var in = new OggAudioStream(Files.newInputStream(path))) {
+                return new SoundSample(in.readAll(), in.getFormat());
+            }
+        }
+
+        @Override
+        public float volume() {
+            return volume;
+        }
+
+        @Override
+        public float pitch() {
+            return pitch;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == this) return true;
+            if (obj == null || obj.getClass() != this.getClass()) return false;
+            var that = (TestRef) obj;
+            return Objects.equals(this.assetPath, that.assetPath) &&
+                    Float.floatToIntBits(this.volume) == Float.floatToIntBits(that.volume) &&
+                    Float.floatToIntBits(this.pitch) == Float.floatToIntBits(that.pitch);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(assetPath, volume, pitch);
+        }
+
+        @Override
+        public String toString() {
+            return "Ref[assetPath=%s, volume=%s, pitch=%s]".formatted(assetPath, volume, pitch);
+        }
+    }
+}
---
+++ src/test/java/work/lclpnet/notica/util/TestUtil.java
@@ -0,0 +1,91 @@
+package work.lclpnet.notica.util;
+
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import work.lclpnet.kibu.assets.AssetManager;
+import work.lclpnet.notica.api.SongDecoder;
+import work.lclpnet.notica.api.data.Song;
+import work.lclpnet.notica.impl.SoundSampleManager;
+import work.lclpnet.notica.impl.UnifiedSoundLoader;
+import work.lclpnet.notica.impl.mix.SongMixer;
+import work.lclpnet.notica.impl.mix.SoundMixer;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.Random;
+
+public class TestUtil {
+
+    public static final Logger logger = LoggerFactory.getLogger(TestUtil.class);
+    public static final AudioFormat AUDIO_FORMAT = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
+            48_000, 16, 2, 4, 48_000, false);
+    private static final String ASSETS_VERSION = "1.21.5";
+
+    private static volatile boolean registryInit = false;
+    private static final AssetManager assetManager = AssetManager.getShared(ASSETS_VERSION);
+    private static final TestSoundRegistry soundRegistry = new TestSoundRegistry(assetManager);
+    private static final UnifiedSoundLoader soundLoader = new UnifiedSoundLoader(AUDIO_FORMAT, logger);
+
+    public static @NotNull Song loadSong(String name, Class<?> clazz) throws IOException {
+        InputStream in = clazz.getResourceAsStream("/songs/" + name);
+        Objects.requireNonNull(in, "Song not found");
+
+        Song song;
+
+        try (in) {
+            song = SongDecoder.parse(in);
+        }
+
+        return song;
+    }
+
+    public static @NotNull SongMixer createSongMixer(Song song, SoundMixer soundMixer) {
+        var random = new Random();
+
+        return new SongMixer(soundMixer, song, () -> random.nextInt(11));
+    }
+
+    public static @NotNull SoundMixer createSoundMixer(Song song) throws IOException {
+        initSoundRegistry();
+
+        var sampleProvider = new TestSoundSampleProvider(song.instruments(), soundRegistry);
+        var sampleManager = new SoundSampleManager(song.instruments(), sampleProvider, soundLoader);
+
+        return new SoundMixer(song, AUDIO_FORMAT, sampleManager, SoundMixer.StereoMode.EQUAL_POWER);
+    }
+
+    public static Path exportSound(ByteBuffer buffer) throws IOException {
+        Path path = Files.createTempFile("notica_test", ".wav");
+
+        exportSound(buffer, path);
+
+        return path;
+    }
+
+    public static void exportSound(ByteBuffer buffer, Path path) throws IOException {
+        try (var out = Files.newOutputStream(path)) {
+            AudioInputStream in = new AudioInputStream(new ByteBufferInputStream(buffer), AUDIO_FORMAT, buffer.limit());
+            AudioSystem.write(in, AudioFileFormat.Type.WAVE, out);
+        }
+    }
+
+    private static void initSoundRegistry() throws IOException {
+        if (registryInit) return;
+
+        synchronized (TestUtil.class) {
+            if (registryInit) return;
+
+            registryInit = true;
+            soundRegistry.init();
+        }
+    }
+}

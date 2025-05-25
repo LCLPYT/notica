bee57165ba06482869a0c16d871f494fa7d2d437
LCLP <lukas@lclpnet.work>
2025-05-25 20:49:36 +0200

    add SongAudioStreamTest::testSameAsContinuous

--- src/test/java/work/lclpnet/notica/impl/mix/SongAudioStreamTest.java
+++ src/test/java/work/lclpnet/notica/impl/mix/SongAudioStreamTest.java
@@ -1,6 +1,7 @@
 package work.lclpnet.notica.impl.mix;
 
 import org.junit.jupiter.api.Test;
+import org.lwjgl.BufferUtils;
 import work.lclpnet.notica.api.data.Song;
 import work.lclpnet.notica.impl.SoundSampleManager;
 import work.lclpnet.notica.util.TestUtil;
@@ -11,15 +12,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static work.lclpnet.notica.util.TestUtil.getBufferByteSize;
+import static work.lclpnet.notica.util.TestUtil.getFrames;
 
 class SongAudioStreamTest {
 
-    private static final boolean EXPORT = false, OPEN = false;
+    private static final boolean EXPORT = true, OPEN = true;
 
     @Test
     void test() throws IOException {
-        Song song = TestUtil.loadSong(Path.of("run", "config", "notica", "songs", "Driftveil City.nbs"));
+        Song song = TestUtil.loadSong("Driftveil City.nbs", SongMixerTest.class);
 
         TestUtil.initSoundRegistry();
 
@@ -38,9 +41,15 @@ class SongAudioStreamTest {
         @SuppressWarnings("resource")
         var stream = new SongAudioStream(TestUtil.AUDIO_FORMAT, soundMixer, songMixer, song, bufferBytes);
 
-        Path dir = Files.createTempDirectory("notica_test");
+        Path dir;
 
-        System.out.println("Exporting into " + dir.toAbsolutePath());
+        if (EXPORT) {
+            dir = Files.createTempDirectory("notica_test");
+
+            System.out.println("Exporting into " + dir.toAbsolutePath());
+        } else {
+            dir = null;
+        }
 
         byte[][] parts = new byte[amount][0];
 
@@ -49,13 +58,7 @@ class SongAudioStreamTest {
 
             if (buf == null) break;
 
-            byte[] array = new byte[buf.limit()];
-
-            for (int j = 0; j < array.length; j++) {
-                array[j] = buf.get();
-            }
-
-            parts[i] = array;
+            parts[i] = asByteArray(buf);
 
             buf.flip();
 
@@ -64,21 +67,106 @@ class SongAudioStreamTest {
             TestUtil.exportSound(buf, dir.resolve(i + ".wav"));
         }
 
-        if (EXPORT) {
-            int totalSize = Arrays.stream(parts).mapToInt(part -> part.length).sum();
-            ByteBuffer buf = ByteBuffer.allocate(totalSize);
+        if (!EXPORT) return;
 
-            for (byte[] part : parts) {
-                buf.put(part);
-            }
+        int totalSize = Arrays.stream(parts).mapToInt(part -> part.length).sum();
+        ByteBuffer buf = ByteBuffer.allocate(totalSize);
 
-            buf.flip();
-
-            TestUtil.exportSound(buf, dir.resolve("combined.wav"));
+        for (byte[] part : parts) {
+            buf.put(part);
         }
 
-        if (OPEN) {
-            Runtime.getRuntime().exec(new String[] {"xdg-open", dir.toAbsolutePath().toString()});
+        buf.flip();
+
+        TestUtil.exportSound(buf, dir.resolve("combined.wav"));
+
+        if (!OPEN) return;
+
+        Runtime.getRuntime().exec(new String[] {"xdg-open", dir.toAbsolutePath().toString()});
+    }
+
+    private byte[] asByteArray(ByteBuffer buf) {
+        byte[] array = new byte[buf.limit()];
+
+        for (int j = 0; j < array.length; j++) {
+            array[j] = buf.get();
         }
+
+        return array;
+    }
+
+    private short[] asShortArray(ByteBuffer buf) {
+        short[] array = new short[buf.limit() / 2];
+
+        for (int j = 0; j < array.length; j++) {
+            array[j] = buf.getShort();
+        }
+
+        return array;
+    }
+
+    @Test
+    void testSameAsContinuous() throws IOException {
+        Song song = TestUtil.loadSong("Driftveil City.nbs", SongMixerTest.class);
+
+        TestUtil.initSoundRegistry();
+
+        SoundSampleManager sampleManager = TestUtil.createSampleManager(song.instruments(), CatmullRomNoteSampler::paddedSample);
+        sampleManager.loadAll();
+
+        float seconds = 1.f;
+        int amount = 5;
+        float volume = 0.5f;
+
+        ByteBuffer reference = reference(song, volume, sampleManager, seconds, amount);
+        ByteBuffer combined = combined(song, volume, sampleManager, seconds, amount);
+
+        short[] reference_array = asShortArray(reference);
+        short[] combined_array = asShortArray(combined);
+
+        assertArrayEquals(reference_array, combined_array, "Combined does not match reference (buffer_size=%s)"
+                .formatted(getBufferByteSize(seconds) / 2));
+    }
+
+    private ByteBuffer combined(Song song, float volume, SoundSampleManager sampleManager, float seconds, int amount) throws IOException {
+        int bufferBytes = getBufferByteSize(seconds);
+
+        SoundMixer soundMixer = TestUtil.createSoundMixer(song, bufferBytes, sampleManager, CatmullRomNoteSampler::new);
+        SongMixer songMixer = TestUtil.createSongMixer(song, soundMixer);
+
+        songMixer.setSongVolume(volume);
+
+        @SuppressWarnings("resource")
+        var stream = new SongAudioStream(TestUtil.AUDIO_FORMAT, soundMixer, songMixer, song, bufferBytes);
+
+        ByteBuffer combined = BufferUtils.createByteBuffer(bufferBytes * amount);
+
+        for (int i = 0; i < amount; i++) {
+            ByteBuffer buf = stream.read(bufferBytes);
+
+            if (buf == null) break;
+
+            combined.put(buf);
+        }
+
+        combined.flip();
+
+        return combined;
+    }
+
+    private ByteBuffer reference(Song song, float volume, SoundSampleManager sampleManager, float seconds, int amount) throws IOException {
+        int bufferBytes = getBufferByteSize(seconds) * amount;
+        int frames = getFrames(bufferBytes);
+
+        SoundMixer soundMixer = TestUtil.createSoundMixer(song, bufferBytes, sampleManager, CatmullRomNoteSampler::new);
+        SongMixer songMixer = TestUtil.createSongMixer(song, soundMixer);
+
+        int startTick = 0;
+        int endTick = startTick + song.tempo().durationTicks(startTick, seconds * amount);
+
+        songMixer.setSongVolume(volume);
+        songMixer.mixTicks(startTick, endTick, 0);
+
+        return soundMixer.applyCompressor(frames);
     }
 }

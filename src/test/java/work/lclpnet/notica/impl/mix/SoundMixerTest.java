81a757e63402b60aade3dbda9eb5bfabb576eba5
LCLP <lukas@lclpnet.work>
2025-06-03 20:09:37 +0200

    fix SoundMixerTest

--- src/test/java/work/lclpnet/notica/impl/mix/SoundMixerTest.java
+++ src/test/java/work/lclpnet/notica/impl/mix/SoundMixerTest.java
@@ -2,6 +2,7 @@ package work.lclpnet.notica.impl.mix;
 
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
+import org.lwjgl.BufferUtils;
 import work.lclpnet.notica.util.TestUtil;
 
 import javax.sound.sampled.AudioFormat;
@@ -9,11 +10,13 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.file.Path;
 
+import static java.lang.Math.*;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.mock;
 
 class SoundMixerTest {
 
-    private static final boolean EXPORT = false, OPEN = false;
+    private static final boolean EXPORT = true, OPEN = true;
 
     @Test
     void mixSample() throws IOException {
@@ -36,11 +39,47 @@ class SoundMixerTest {
 
         mixer.mixSample(sine, totalFrames, 0, 0, mixer.getScope());
 
-        ByteBuffer buf = mixer.applyClamping(totalFrames, mixer.getScope());
+        ByteBuffer combined = BufferUtils.createByteBuffer(totalSamples * 2);
+
+        int iterations = (int) ceil(totalFrames / (double) bufferFrames);
+
+        for (int i = 0; i < iterations; i++) {
+            ByteBuffer buf = mixer.applyClamping(bufferFrames, mixer.getScope());
+
+            combined.put(buf);
+
+            mixer.advanceBuffer();
+        }
+
+        combined.flip();
+
+        // left channel
+        for (int i = 0; i < totalFrames; i++) {
+            float v = sine[i];
+            short expected = (short) max(Short.MIN_VALUE, min(Short.MAX_VALUE, v * Short.MAX_VALUE));
+
+            short actual = combined.getShort(i * 4);
+
+            if (expected == actual) continue;
+
+            fail("Mismatch in left channel at index [%s]: Expected <%s> but was <%s>".formatted(i, expected, actual));
+        }
+
+        // right channel
+        for (int i = 0; i < totalFrames; i++) {
+            float v = sine[i + totalFrames];
+            short expected = (short) max(Short.MIN_VALUE, min(Short.MAX_VALUE, v * Short.MAX_VALUE));
+
+            short actual = combined.getShort(i * 4 + 2);
+
+            if (expected == actual) continue;
+
+            fail("Mismatch in left channel at index [%s]: Expected <%s> but was <%s>".formatted(i, expected, actual));
+        }
 
         if (!EXPORT) return;
 
-        Path path = TestUtil.exportSound(buf);
+        Path path = TestUtil.exportSound(combined);
 
         System.out.println(path.toAbsolutePath());
 

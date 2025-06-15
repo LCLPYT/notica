c869dd7f328f13b2a0a7f33891db68ca83793d07
LCLP <lukas@lclpnet.work>
2025-06-15 20:43:03 +0200

    undo cubic interpolation for java internal resampling as resulting samples seem to be broken

--- src/client/java/work/lclpnet/notica/impl/ClientMusicBackend.java
+++ src/client/java/work/lclpnet/notica/impl/ClientMusicBackend.java
@@ -40,8 +40,7 @@ public class ClientMusicBackend {
     private final Map<Identifier, SongPlayback> playing = new HashMap<>();
     private final DirectSoundManager directSoundManager = new DirectSoundManager();
     private final AudioFormat unifiedAudioFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
-            48_000, 16, 2, 4, 48_000, false,
-            Map.of("interpolation", "cubic"));
+            48_000, 16, 2, 4, 48_000, false);
     private final UnifiedSoundLoader unifiedSoundLoader;
 
     public ClientMusicBackend(ClientSongRepository songRepository, InstrumentSoundProvider soundProvider,

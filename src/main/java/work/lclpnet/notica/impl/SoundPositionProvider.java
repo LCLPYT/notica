0db420218f8f1a9cb5436dfca509b0543fb1a334
LCLP <lukas@lclpnet.work>
2026-04-25 02:24:18 +0200

    fix: proper right vector for speaker offset directly on player

--- src/main/java/work/lclpnet/notica/impl/SoundPositionProvider.java
+++ src/main/java/work/lclpnet/notica/impl/SoundPositionProvider.java
@@ -52,12 +52,17 @@ public interface SoundPositionProvider {
 
             double length = sqrt(dx * dx + dz * dz);
 
-            double rightX = 0;
-            double rightZ = 0;
+            double rightX;
+            double rightZ;
 
             if (length > 0) {
                 rightX = dz / length;
                 rightZ = -dx / length;
+            } else {
+                // in case the sourcePos is exactly at the player, use player look angle to determine right vector
+                Vec3 right = player.getLookAngle().cross(Vec3.Y_AXIS);
+                rightX = right.x();
+                rightZ = right.z();
             }
 
             double radius = speaker.radius();

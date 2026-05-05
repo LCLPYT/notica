0a9d03f74c97a813ba111f64a72b4fddcfc911a3
LCLP <lukas@lclpnet.work>
2026-05-05 19:55:31 +0200

    feat: make cloth-config optional at runtime

--- src/client/java/work/lclpnet/notica/NoticaModMenu.java
+++ src/client/java/work/lclpnet/notica/NoticaModMenu.java
@@ -2,12 +2,17 @@ package work.lclpnet.notica;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.fabricmc.loader.api.FabricLoader;
 import work.lclpnet.notica.config.ConfigScreenBuilder;
 
 public class NoticaModMenu implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        if (!FabricLoader.getInstance().isModLoaded("cloth-config")) {
+            return ModMenuApi.super.getModConfigScreenFactory();
+        }
+
         return parent -> NoticaClientInit.configManager()
                 .map(ConfigScreenBuilder::new)
                 .map(builder -> builder.create(parent))
